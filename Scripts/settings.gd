e9bd7cb57266ea073d36b3aaf39ae77e524ce59e
Pixeleyesd <juliushwashere@gmail.com>
2026-06-30 18:19:00 +1200

    mostly broken fade out / in transitions between scenes

--- Scripts/settings.gd
+++ Scripts/settings.gd
@@ -1,15 +1,20 @@
 extends Node2D
 
-
+@onready var animation_player = get_node("ColorRect2/AnimationPlayer")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	get_node("ColorRect").show()
+	get_node("ColorRect2").hide()
+	await get_tree().create_timer(0.2).timeout
+	get_node("ColorRect").hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_button_pressed() -> void:
+	get_node("ColorRect2").show()
+	animation_player.play("fade out")
 	get_tree().change_scene_to_file("res://Scenes/title_screen_3d.tscn")
--- Scripts/titlescreen.gd
+++ Scripts/titlescreen.gd
@@ -2,20 +2,29 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
 func _on_start_button_pressed() -> void:
+	get_node("ColorRect/AnimationPlayer").play("fade out 2")
+	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
 
 func _on_button_settings_pressed() -> void:
+	get_node("ColorRect/AnimationPlayer").play("fade out 2")
+	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
 
 func _on_button_quit_pressed() -> void:
+	get_node("ColorRect").show()
+	get_node("ColorRect/AnimationPlayer").play("fade out 2")
+	await get_tree().create_timer(0.2).timeout
 	get_tree().quit()
 
 func _on_button_credits_pressed() -> void:
+	get_node("ColorRect").show()
+	get_node("ColorRect/AnimationPlayer").play("fade out 2")
+	await get_tree().create_timer(0.2).timeout
 	get_tree().change_scene_to_file("res://Scenes/credits.tscn")

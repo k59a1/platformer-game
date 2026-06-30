4599e575302918a1ddba1b7302206e72866849e9
Pixeleyesd <juliushwashere@gmail.com>
2026-06-30 21:09:04 +1200

    Coyote time maybe work cant test

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,22 +1,28 @@
 extends CharacterBody2D
-
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const COYOTE_TIME = 0.15
 
+var coyote_timer := 0.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		coyote_timer = COYOTE_TIME
+	else:
+		coyote_timer -= delta
 
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and coyote_timer > 0.0:
+		velocity.y = JUMP_VELOCITY
+		coyote_timer = 0.0
+	
+	if Input.is_action_just_pressed("ui_up") and coyote_timer > 0.0:
+		velocity.y = JUMP_VELOCITY
+		coyote_timer = 0.0
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
@@ -24,5 +30,4 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
